612cda2e300462b14ae652bd8e00284f9fcc2fae
Fred Stakem <fstakem@gmail.com>
2014-09-14 13:46:16 -0400

    Initial commit.

---
+++ src/exponential_sim.R
@@ -0,0 +1,15 @@
+# <<<<--------------------------------------------<>-------------------------------------------->>>>
+#
+#       exponential_sim.R
+#       By: Fredrick Stakem
+#       Date: 9.14.14
+#
+#       Purpose:    To simulate the exponential distribution to learn basic statistics
+#                   for the class Statistical Inference from John Hopkins offered
+#                   through Coursera.
+#
+# <<<<--------------------------------------------<>-------------------------------------------->>>>
+
+# Libraries
+
+#
